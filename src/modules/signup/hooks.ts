eeca3bc7b3f9b1e9dc99457e3cbc130e8dd61103
Lucas Santos <lucas201273@gmail.com>
2025-01-01 09:27:15 -0300

    feat: finish signup module

--- src/modules/signup/hooks.ts
+++ src/modules/signup/hooks.ts
@@ -4,7 +4,8 @@ import { schema, service,} from './index'
 import { useState, Dispatch, SetStateAction } from 'react'
 import { useForm, UseFormReturn } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
-
+import { toast } from 'sonner'
+import { useRouter } from 'next/navigation'
 interface SignUpHookProps{
   loading:{
     loading: boolean
@@ -16,6 +17,7 @@ interface SignUpHookProps{
 
 
 export const useSignUpHook = ():SignUpHookProps => {
+  const router = useRouter();
   const [loading, setLoading] = useState<boolean>(false);
 
   const form = useForm<AddAccountParams>({
@@ -31,8 +33,11 @@ export const useSignUpHook = ():SignUpHookProps => {
     try{
       const payload = form.getValues();
       await service.add(payload)
-      console.log(payload)
+      toast.success('Usuário cadastrado com successo!')
+      toast.info('Você será redirecionado para a tela de login em instantes.')
+      router.push('/')
     }catch(error:any){
+      toast.error('Ocorreu um erro ao realizar o cadastro.')
       console.log(error);
 
     }finally{
--- src/modules/signup/signup-service.ts
+++ src/modules/signup/signup-service.ts
@@ -1,10 +1,12 @@
 import { User } from '@app/domain/models/user';
 import {AddAccount, AddAccountParams} from '@app/domain/usecases/signup'
-
+import { service as apiService } from '@app/config'
+import { validation } from '@app/utils/validators'
 class SignUpService implements AddAccount{
   async add(params: AddAccountParams): Promise<User> {
-    console.log({ params })
-    throw new Error('Method not implemented.');
+    const response = await apiService.post("/users", params);
+    validation.validateResponse({statusCode:201, response, displayErrorMessage: 'Não foi possivel realizar o cadastro'});
+    return response.data;
   }
 
 }
